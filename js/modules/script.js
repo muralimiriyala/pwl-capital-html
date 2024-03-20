3842e80dd9af0ca0ed508f5ba50ecf168cc297ab
unknown <ramlachu@jbkinfotech.com>
2024-03-20 20:03:31 +0530

    footer responsive done

--- js/modules/script.js
+++ js/modules/script.js
@@ -5,4 +5,18 @@ jQuery(function($){
         const _scroll = window.scrollY;
         _scroll >= 4 ? header.addClass("sticky") : header.removeClass("sticky");
     }
+
 });
+
+// jQuery(function($){
+//     $('.footer-nav span').click(function() {      
+//        $('.footer-nav-cont').toggle();
+//     })
+// });
+
+// jQuery(document).ready(function(){
+//     jQuery(".test").on("click", function(){
+//         jQuery("ul.footer-nav-cont").addClass("open");
+//     });
+// });
+
