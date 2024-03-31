63744b496a50435707870124c2e8657192fe3945
MuraliJbk <murali@jbkinfotech.com>
2024-03-31 21:54:26 +0530

    paths

--- js/modules/myscript.js
+++ js/modules/myscript.js
@@ -14,19 +14,27 @@ jQuery(function($){
     };
     
 
-    $('.footer-list').click(function() {     
+    $('.footer-list').on("click", function(){
         $(this).toggleClass("open");
         $(this).siblings('.footer-list').removeClass("open");
         $(this).find('.footer-links').slideToggle(900);
         $(this).siblings('.footer-list').find('.footer-links').slideUp(900);  
      })
 
-    $('.accordion-item').click(function() {     
+    $('.accordion-item').on("click", function(){
         $(this).toggleClass("open");
         $(this).siblings('.accordion-item').removeClass("open");
         $(this).find('.accordion-content').slideToggle(900);
         $(this).siblings('.accordion-item').find('.accordion-content').slideUp(900);  
-     })
+     });
+
+
+     $(".humburger-btn").on("click", function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $(".header-right").toggleClass("open");
+
+     });
 
 });
 
