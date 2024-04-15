6606ee279b78a73fdc7e62e5c7d14ea2aca52b78
unknown <ramlachu@jbkinfotech.com>
2024-04-15 12:27:22 +0530

    006

--- js/modules/team-script.js
+++ js/modules/team-script.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
     const exploreLink = $(".team-item");
-    exploreLink.hover(function(e){
+    exploreLink.mouseover(function(e){
         e.preventDefault();
         _self = $(this);
         _self.addClass("active");
@@ -8,6 +8,10 @@ jQuery(document).ready(function($){
         const exploreName = _self.data("name");
         $(".team-content-main").fadeOut(100);
         $(".team-content-main[data-value="+ exploreName +"]").fadeIn(600);
+    });
+    exploreLink.mouseleave(function(e){
+        e.preventDefault();
+        $(".team-content-main").fadeOut(100);
     });
 
 });
