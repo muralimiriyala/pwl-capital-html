dc27904f3467d8f977d3cd7b8d2ccec3e5ac5b69
unknown <ramlachu@jbkinfotech.com>
2024-05-25 20:03:20 +0530

    correction

--- js/modules/team-script.js
+++ js/modules/team-script.js
@@ -1,3 +1,20 @@
+// jQuery(document).ready(function($){
+//     const exploreLink = $(".team-item");
+//     exploreLink.mouseover(function(e){
+//         e.preventDefault();
+//         _self = $(this);
+//         _self.addClass("active");
+//         _self.siblings().removeClass("active");
+//         const exploreName = _self.data("name");
+//         $(".team-content-main").fadeOut(100);
+//         $(".team-content-main[data-value="+ exploreName +"]").fadeIn(600);
+//     });
+//     exploreLink.mouseleave(function(e){
+//         e.preventDefault();
+//         $(".team-content-main").fadeOut(100);
+//     });
+
+// });
 jQuery(document).ready(function($){
     const exploreLink = $(".team-item");
     exploreLink.mouseover(function(e){
@@ -5,12 +22,15 @@ jQuery(document).ready(function($){
         _self = $(this);
         _self.addClass("active");
         _self.siblings().removeClass("active");
+        _self.parent().addClass("active");
         const exploreName = _self.data("name");
         $(".team-content-main").fadeOut(100);
         $(".team-content-main[data-value="+ exploreName +"]").fadeIn(600);
     });
     exploreLink.mouseleave(function(e){
         e.preventDefault();
+        _self.removeClass("active");
+        _self.parent().removeClass("active");
         $(".team-content-main").fadeOut(100);
     });
 
