becb85dcc0b7ba0a202cfff7e876e63cdbb15db0
MuraliJbk <murali@jbkinfotech.com>
2024-05-28 19:42:38 +0530

    28-05-2024

--- js/modules/team-script.js
+++ js/modules/team-script.js
@@ -15,23 +15,33 @@
 //     });
 
 // });
-jQuery(document).ready(function($){
-    const exploreLink = $(".team-item");
-    exploreLink.mouseover(function(e){
-        e.preventDefault();
-        _self = $(this);
-        _self.addClass("active");
-        _self.siblings().removeClass("active");
-        _self.parent().addClass("active");
-        const exploreName = _self.data("name");
-        $(".team-content-main").fadeOut(100);
-        $(".team-content-main[data-value="+ exploreName +"]").fadeIn(600);
-    });
-    exploreLink.mouseleave(function(e){
-        e.preventDefault();
-        _self.removeClass("active");
-        _self.parent().removeClass("active");
-        $(".team-content-main").fadeOut(100);
-    });
+// jQuery(document).ready(function($){
+//     const exploreLink = $(".team-item");
+//     exploreLink.mouseover(function(e){
+//         e.preventDefault();
+//         const _self = $(this);
+//         const exploreName = _self.data("name");
 
-});
+//         // Add 'active' class to the current item and its parent
+//         _self.addClass("active");
+//         _self.siblings().removeClass("active");
+//         _self.parent().addClass("active");
+
+//         // Fade out all content and fade in the target content
+//         $(".team-content-main").stop(true, true).fadeOut(100, function() {
+//             $(".team-content-main[data-value="+ exploreName +"]").fadeIn(600);
+//         });
+//     });
+
+//     exploreLink.mouseleave(function(e){
+//         e.preventDefault();
+//         const _self = $(this);
+
+//         // Remove 'active' class from the current item and its parent
+//         _self.removeClass("active");
+//         _self.parent().removeClass("active");
+
+//         // Fade out all content
+//         $(".team-content-main").stop(true, true).fadeOut(100);
+//     });
+// });
