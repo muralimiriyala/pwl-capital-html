e75f7f68794745eeda567dda2a7aba0649c72048
MuraliJbk <murali@jbkinfotech.com>
2024-05-29 16:30:33 +0530

    29-05-2024

--- js/modules/team-script.js
+++ js/modules/team-script.js
@@ -1,52 +1,22 @@
-// jQuery(document).ready(function($){
-//     const exploreLink = $(".team-item");
-//     exploreLink.mouseover(function(e){
-//         e.preventDefault();
-//         _self = $(this);
-//         _self.addClass("active");
-//         _self.siblings().removeClass("active");
-//         const exploreName = _self.data("name");
-//         $(".team-content-main").fadeOut(100);
-//         $(".team-content-main[data-value="+ exploreName +"]").fadeIn(600);
-//     });
-//     exploreLink.mouseleave(function(e){
-//         e.preventDefault();
-//         $(".team-content-main").fadeOut(100);
-//     });
-
-// });
-// jQuery(document).ready(function($){
-//     const exploreLink = $(".team-item");
-//     exploreLink.mouseover(function(e){
-//         e.preventDefault();
-//         const _self = $(this);
-//         const exploreName = _self.data("name");
-
-//         // Add 'active' class to the current item and its parent
-//         _self.addClass("active");
-//         _self.siblings().removeClass("active");
-//         _self.parent().addClass("active");
-
-//         // Fade out all content and fade in the target content
-//         $(".team-content-main").stop(true, true).fadeOut(100, function() {
-//             $(".team-content-main[data-value="+ exploreName +"]").fadeIn(600);
-//         });
-//     });
-
-//     exploreLink.mouseleave(function(e){
-//         e.preventDefault();
-//         const _self = $(this);
-
-//         // Remove 'active' class from the current item and its parent
-//         _self.removeClass("active");
-//         _self.parent().removeClass("active");
-
-//         // Fade out all content
-//         $(".team-content-main").stop(true, true).fadeOut(100);
-//     });
-// });
-
 jQuery(document).ready(function(){
+    if(jQuery(window).width() >= 1024){
+        const $teamlists = $(".team-lists");
+        const $teamlist = $teamlists.find(".team-list");
+        console.log($teamlist, "test")
+        exploreLink.mouseover(function(e){
+            e.preventDefault();
+            _self = $(this);
+            _self.addClass("active");
+            _self.siblings().removeClass("active");
+            const exploreName = _self.data("name");
+            $(".team-content-main").fadeOut(100);
+            $(".team-content-main[data-value="+ exploreName +"]").fadeIn(600);
+        });
+        exploreLink.mouseleave(function(e){
+            e.preventDefault();
+            $(".team-content-main").fadeOut(100);
+        });
+    }
     if(jQuery(window).width() <= 1023){
         jQuery(".team-item").on('click', function(e){
             e.preventDefault();
