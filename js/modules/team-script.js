6b0ccdcda076b3edca8d38be3a3dc845da74616d
MuraliJbk <murali@jbkinfotech.com>
2024-05-31 21:47:19 +0530

    31-05-2024

--- js/modules/team-script.js
+++ js/modules/team-script.js
@@ -1,33 +1,56 @@
-
 jQuery(document).ready(function() {
-    if (window.matchMedia('(min-width: 1024px)').matches) {
-        const $teamlists = jQuery(".team-item");
-        $teamlists.each(function() {
-            const $teamlist = jQuery(this);
-            $teamlist.on('click', function(e) {
-                    console.log($teamlist);
-                e.preventDefault();
-
-                const $self = jQuery(this);
-                const $teamName = $self.data("name");
+    if (jQuery(window).width() >= 1024) {
+        jQuery(".team-lists").each(function() {
+            let $teamItems = jQuery(this).find(".team-item");
+    
+            $teamItems.on('click', function(e) {
+                e.preventDefault(); // Prevent default action if it's an anchor tag
                 
-                $self.parent().siblings(".team-lists").find(".team-item").toggleClass("team-row-hide");
-                $self.siblings(".team-item").addClass("team-hide");
-                $self.removeClass("team-hide");
-         
-                jQuery(".team-content-main").not(".team-content-main[data-value='" + $teamName + "']").hide();
-                jQuery(".team-content-main[data-value=" + $teamName + "]").fadeToggle(1000);
+                let $this = jQuery(this);
+                let isActive = $this.hasClass('team-active');
+                
+                // If the clicked item is already active, deactivate all
+                if (isActive) {
+                    $teamItems.removeClass("team-hide team-active");
+                    $this.closest('.team-lists').siblings(".team-lists").find(".team-item").removeClass("team-hide");
+                } else {
+                    // Add 'team-hide' class to all siblings and sibling items
+                    $this.closest('.team-lists').siblings(".team-lists").find(".team-item").addClass("team-hide");
+                    $this.siblings().addClass("team-hide");
+                    $this.addClass("team-active").removeClass("team-hide");
+                }
+
+                const $teamName = $this.data("name");
+                if (isActive) {
+                    jQuery(".team-content-main[data-value='" + $teamName + "']").slideUp(800);
+                } else {
+                    jQuery(".team-content-main").slideUp(800);
+                    jQuery(".team-content-main[data-value='" + $teamName + "']").slideDown(800);
+                }
             });
         });
     } 
     if (jQuery(window).width() <= 1023) {
         jQuery(".team-list").on('click', function(e) {
             e.preventDefault();
-            jQuery(this).parent(".team-item").closest(".team-lists").siblings(".team-lists").find(".team-item").children(".team-list").toggleClass("team-hide");
-            jQuery(this).parent().siblings(".team-item").find(".team-list").toggleClass("team-hide");
-            jQuery(this).toggleClass("team-active");
+
+            const $this = jQuery(this).parent(); // Get the parent .team-item
+            const isActive = $this.hasClass('team-active');
+
+            // If the clicked item is already active, deactivate all
+            if (isActive) {
+                $this.removeClass("team-hide team-active");
+                $this.closest('.team-lists').siblings(".team-lists").find(".team-item").removeClass("team-hide");
+            } else {
+                // Add 'team-hide' class to all siblings and sibling items
+                $this.closest('.team-lists').siblings(".team-lists").find(".team-item").addClass("team-hide");
+                $this.siblings().addClass("team-hide");
+                $this.addClass("team-active").removeClass("team-hide");
+            }
+
             jQuery(".team-content-main").not(jQuery(this).siblings(".team-content-main")).slideUp(800);
             jQuery(this).siblings(".team-content-main").slideToggle(800);
+
         });
     }
 });
