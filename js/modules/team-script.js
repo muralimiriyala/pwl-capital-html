5c6e77c8ac3c018170a6c5f3589e8d367efebec4
MuraliJbk <murali@jbkinfotech.com>
2024-05-31 17:23:07 +0530

    31-05-2024

--- js/modules/team-script.js
+++ js/modules/team-script.js
@@ -1,71 +1,37 @@
-jQuery(document).ready(function() {
-    if (jQuery(window).width() >= 1024) {
-        const $teamlists = $(".team-lists");
-        $teamlists.each(function() {
-            const $teamlist = jQuery(this).find(".team-list");
-            function handleMouseEnter(e) {
-                e.preventDefault();
-        
-                const $teamName = jQuery(this).parent().data("name");
-                const $teamHeight = $teamlist.outerHeight(true);
-                const $contentHeight = $(".team-content-main[data-value=" + $teamName + "]").outerHeight(true);
-                const $content = $(".team-content-main[data-value=" + $teamName + "]");
-    
-               $teamlist.css({
-                    'position': 'relative',
-                    'z-index': '800',
-                    'padding-bottom': $contentHeight + 120 + 'px',
-                });
-                $content.css({
-                    'top': $teamHeight + 'px',
-                });
-        
-                $(".team-content-main").stop(true, true).hide(100);
-                $(".team-content-main[data-value=" + $teamName + "]").stop(true, true).fadeIn(800);
-            }
-        
-            function handleMouseLeave(e) {
-                e.preventDefault();
 
-
-                $teamlist.css('padding-bottom', '51px');
-                $(".team-content-main").stop(true, true).fadeOut(800);
-            }
-        
-            $teamlist.on('mouseenter', handleMouseEnter);
-            $teamlist.on('mouseleave', handleMouseLeave);
-            
+function resize() {
+    if (window.matchMedia('(min-width: 1024px)').matches) {
+        jQuery(".team-list").on('click', function(e) {
+            e.preventDefault();
+            jQuery(this).parent(".team-item").closest(".team-lists").siblings(".team-lists").find(".team-item").children(".team-list").toggleClass("team-hide");
+            jQuery(this).parent().siblings(".team-item").find(".team-list").toggleClass("team-hide");
+            jQuery(this).toggleClass("team-active");
+            const $teamName = jQuery(this).parent().data("name");
+            jQuery(".team-content-main").hide(100);
+            jQuery(".team-content-main[data-value=" + $teamName + "]").fadeToggle(400);
         });
-        
-        jQuery(".team-lists").each(function(){
-            let $teamItems = jQuery(this).find(".team-item");
-            $teamItems.hover(
-                function() {
-                    // Mouse enter
-                    jQuery(this).parent().siblings(".team-lists").find(".team-item").addClass("team-hide");
-                    jQuery(this).siblings().addClass("team-hide");
-                    jQuery(this).addClass("team-active");
-                }, 
-                function() {
-                    // Mouse leave
-                    jQuery(this).parent().siblings(".team-lists").find(".team-item").removeClass("team-hide");
-                    jQuery(this).siblings().removeClass("team-hide");
-                    jQuery(this).removeClass("team-active");
-                }
-            );
-        });
-        
-    }
-
-    if (jQuery(window).width() <= 1023) {
+    } else {
         jQuery(".team-list").on('click', function(e) {
             e.preventDefault();
             jQuery(this).parent(".team-item").closest(".team-lists").siblings(".team-lists").find(".team-item").children(".team-list").toggleClass("team-hide");
             jQuery(this).parent().siblings(".team-item").find(".team-list").toggleClass("team-hide");
             jQuery(this).toggleClass("team-active");
 
+            alert("murali")
             jQuery(".team-content-main").not(jQuery(this).siblings(".team-content-main")).slideUp(800);
             jQuery(this).siblings(".team-content-main").slideToggle(800);
         });
     }
-});
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    resize();
+  });
+  
+  window.onload = function () {
+    resize();
+  };
+  window.onresize = function () {
+    resize();
+  };
+  
