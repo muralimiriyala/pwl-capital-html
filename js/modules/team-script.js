659479fcc413f0fcd857f7b2571830dfae986725
MuraliJbk <murali@jbkinfotech.com>
2024-05-31 20:08:31 +0530

    31-05-2024

--- js/modules/team-script.js
+++ js/modules/team-script.js
@@ -1,19 +1,19 @@
 
 jQuery(document).ready(function() {
     if (window.matchMedia('(min-width: 1024px)').matches) {
-        const $teamlists = jQuery(".team-lists");
+        const $teamlists = jQuery(".team-item");
         $teamlists.each(function() {
-            const $teamlist = jQuery(this).find(".team-list");
-                $teamlist.on('click', function(e) {
+            const $teamlist = jQuery(this);
+            $teamlist.on('click', function(e) {
+                    console.log($teamlist);
                 e.preventDefault();
 
-
                 const $self = jQuery(this);
-                const $teamName = jQuery(this).parent().data("name");
+                const $teamName = $self.data("name");
                 
-                jQuery('.team-list').parent().toggleClass('team-hide');
-                jQuery(".team-item[data-name=" + $teamName + "]").removeClass("team-hide");
-
+                $self.parent().siblings(".team-lists").find(".team-item").toggleClass("team-row-hide");
+                $self.siblings(".team-item").toggleClass("team-hide");
+         
                 jQuery(".team-content-main").not(".team-content-main[data-value='" + $teamName + "']").hide();
                 jQuery(".team-content-main[data-value=" + $teamName + "]").fadeToggle(1000);
             });
