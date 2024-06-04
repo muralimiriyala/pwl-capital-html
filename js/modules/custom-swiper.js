9a68b195fc8f35610cf3989bf49cc0a0ae6f3c5c
MuraliJbk <murali@jbkinfotech.com>
2024-06-04 14:06:32 +0530

    31-05-2024

--- js/modules/custom-swiper.js
+++ js/modules/custom-swiper.js
@@ -1,23 +1,62 @@
 
 (function($){
-  const $link = $("ul.how-can-links li a");
-  console.log($link)
-
+  
   const $howcanslider = $(".how-can-slider");
+$howcanslider.on('init', function(event, slick, currentSlide) {
+  var
+    cur = $(slick.$slides[slick.currentSlide]),
+    next = cur.next(),
+    prev = cur.prev();
+  prev.addClass('slick-sprev');
+  next.addClass('slick-snext');
+  cur.removeClass('slick-snext').removeClass('slick-sprev');
+  slick.$prev = prev;
+  slick.$next = next;
+}).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+  //console.log('beforeChange');
+  var
+    cur = $(slick.$slides[nextSlide]);
+  //console.log(slick.$prev, slick.$next);
+  slick.$prev.removeClass('slick-sprev');
+  slick.$next.removeClass('slick-snext');
+  next = cur.next(),
+    prev = cur.prev();
+  prev.prev();
+  prev.next();
+  prev.addClass('slick-sprev');
+  next.addClass('slick-snext');
+  slick.$prev = prev;
+  slick.$next = next;
+  cur.removeClass('slick-next').removeClass('slick-sprev');
+});
+
   $howcanslider.slick({
+    slidesPerRow: 1,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     dots: true,
     speed: 1500,
-    infinite: false,
     autoplay: false,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '0',
+    swipe: true,
     // variableWidth: true,
+    customPaging: function(slider, i) {
+      return '';
+    },
   });
+  const $link = $("ul.how-can-links li a");
   $link.on("click", function(e){
     e.preventDefault();
-    var slideno = jQuery(this).data('slide');
-    $contentSlider.slick('slickGoTo', slideno - 1);
+    const $this = $(this);
+    $(this).parent("li").siblings().find("a").removeClass("slick-active");
+    // $(this).not("ul.how-can-links li a").removeClass("slick-active");
+    $this.toggleClass("slick-active");
+    var slideno = $this.data('link-slide');
+    console.log(slideno)
+    $howcanslider.slick('slickGoTo', slideno - 1);
   });
 
 }(jQuery));
