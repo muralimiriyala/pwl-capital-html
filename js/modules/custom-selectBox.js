3665fdc468bed7ba95efa6442852a9a5324987b2
MuraliJbk <murali@jbkinfotech.com>
2024-06-11 17:23:08 +0530

    06-06-2024

--- js/modules/custom-selectBox.js
+++ js/modules/custom-selectBox.js
@@ -27,17 +27,41 @@ jQuery('select').selectBox().each(function(){
 });
 jQuery('select').on('change', function(){
     let selectedValue = $(this).val();
-    console.log(selectedValue)
-    if (selectedValue === "value_to_hide_murali") {
-        jQuery('div#frm_error_field_s15zr.error').hide();
+    jQuery('div#frm_error_field_s15zr').show();
+    if (selectedValue === selectedValue) {
+        jQuery('div#frm_error_field_s15zr').hide();
     } else {
-        jQuery('div#frm_error_field_s15zr.error').show();
+        jQuery('div#frm_error_field_s15zr').show();
     }
 });
 
-
-
-
+function checkMyClick(){
+    jQuery("body .financial-goals-form .frm_forms .frm_form_fields .frm_form_field.vertical_radio input[type=checkbox]").on("click", function(e){
+        const _this = jQuery(this);
+        _this.parent().toggleClass('open');
+    });
+    jQuery("body .financial-goals-form .frm_forms .frm_form_fields .frm_form_field .frm_checkbox input[type=checkbox]:checked").each(function() {
+        const _this = jQuery(this);
+        let isChecked = jQuery(this).prop("checked");
+        if(isChecked){
+            _this.parent().addClass('open');
+        }
+    });
+    jQuery('body .frm_forms .frm_form_fields .frm_form_field.vertical_radio input[type="radio"]').on("click", function(e){
+        const _this = jQuery(this);
+        _this.parent().addClass('open');
+        _this.parent().closest(".frm_radio").siblings().find("label").removeClass('open');
+    });
+}
+checkMyClick();
+const observer = new MutationObserver(function(mutationsList) {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            checkMyClick();
+        }
+    }
+});
+observer.observe(document.body, { childList: true, subtree: true });
 
 
 
