9c04752d1cc1e663751cb2fad86ff3f4863103ba
unknown <ramlachu@jbkinfotech.com>
2024-06-13 19:09:31 +0530

    correction

--- js/modules/custom-selectBox.js
+++ js/modules/custom-selectBox.js
@@ -3,10 +3,6 @@
 
 jQuery(document).ready(function($){ 
     function initSelectBox() {
-        // Assuming you have a select box with id "mySelectBox"
-        const selectBox = document.getElementById('signin');
-        selectBox.options[0].classList.add('first-sign-option');
-
 
         $('select').selectBox({
             keepInViewport: false,
