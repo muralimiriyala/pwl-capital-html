e32f4c0adc61df0351eebfeb3b60971ba54e4aba
MuraliJbk <murali@jbkinfotech.com>
2024-06-14 17:08:12 +0530

    06-06-2024

--- js/modules/custom-selectBox.js
+++ js/modules/custom-selectBox.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($){
         $('select').selectBox({
             keepInViewport: false,
             menuSpeed: 'slow',
-            mobile: true,
+            mobile:  true,
             hideOnWindowScroll: true,
         });
         $(".selectBox, .selectBox-dropdown .selectBox-label").removeAttr('style');
